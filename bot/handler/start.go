18a877c9ec15099e263a3d6544db8b553a5b096a
rebecca554owen <rebecca554owen@gmail.com>
2025-04-22 10:21:07 +0800

    test

--- bot/handler/start.go
+++ bot/handler/start.go
@@ -5,10 +5,12 @@ import tele "gopkg.in/telebot.v4"
 // handleStart 处理 /start 命令
 func (h *Handler) handleStart(c tele.Context) error {
 	text := `欢迎使用 XBoard Bot！
-版本: 1.0.0
+版本: 1.1.0
 作者: 周宇航
 
-可用命令：
-/bind <订阅地址> - 绑定 XBoard 账户`
+可用命令:
+/bind <订阅地址> - 绑定 XBoard 账户
+/unbind - 解绑 XBoard 账户
+/subinfo - 查看订阅信息`
 	return c.Send(text)
 }
